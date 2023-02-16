5aa2b0f7c78f95412d8cea668fb0e0d00a3d507e
Vladislav_Global <vladalexkirbut2003@gmail.com>
2023-02-16 22:27:27 +0300

    Added to IntList class 2 constructors

---
+++ src/by/teachmeslills/kirbut/listutils/IntList.java
@@ -0,0 +1,14 @@
+package by.teachmeslills.kirbut.listutils;
+
+public class IntList {
+
+    private int[] arrayOfNumbers;
+
+    public IntList() {
+        arrayOfNumbers = new int[]{};
+    }
+
+    public IntList(int[] arrayOfNumbers) {
+        this.arrayOfNumbers = arrayOfNumbers;
+    }
+}
