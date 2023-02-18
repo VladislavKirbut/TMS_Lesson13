ef43116a86aa1db7eccf206703794d9236023957
Vladislav_Global <vladalexkirbut2003@gmail.com>
2023-02-18 14:49:04 +0300

    Added sort to IntList class

--- src/by/teachmeslills/kirbut/listutils/IntList.java
+++ src/by/teachmeslills/kirbut/listutils/IntList.java
@@ -111,4 +111,16 @@ public class IntList {
 
         return -1;
     }
+
+    public void sort() {
+        for (int i = 0; i < this.arrayOfNumbers.length - 1; i++) {
+            for (int j = 0; j < this.arrayOfNumbers.length - i - 1; j++) {
+                if (arrayOfNumbers[j] > arrayOfNumbers[j + 1]) {
+                    int t = arrayOfNumbers[j];
+                    arrayOfNumbers[j] = arrayOfNumbers[j + 1];
+                    arrayOfNumbers[j + 1] = t;
+                }
+            }
+        }
+    }
 }
